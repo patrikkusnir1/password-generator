9d0c2a5ba0888c81c9f2bd2c253eeaa2846cfd8f
patrikkusnir1 <patrikkusnir1@gmail.com>
2025-10-08 17:58:30 +0300

    fix add symbols to password feature

--- index.js
+++ index.js
@@ -15,31 +15,47 @@ let symbolsCheck = document.getElementById("symbols");
 function generatePassword() {
   passwordEl1.textContent = "";
   passwordEl2.textContent = "";
+
   const passwordLength = parseInt(charactersInput.value, 10); // take value from input
-  const symbolsValue = symbolsCheck.value;
 
   // check input field for valid password number
   if (isNaN(passwordLength) || passwordLength <= 0) {
-    alert("Type valid length of password")
+    alert("Type valid length of password");
     return;
   }
-  // with symbols
-  if (symbolsCheck.checked) {
-      for (let i = 0; i < passwordLength; i++) {
-        let randomIndex1 = Math.floor(Math.random() * characters.length);
-        let randomIndex2 = Math.floor(Math.random() * characters.length);
-        passwordEl1.textContent += characters[randomIndex1];
-        passwordEl2.textContent += characters[randomIndex2];
+
+  // Loop twice to generate two passwords
+  for (let i = 0; i < 2; i++) {
+    let passwordArray = [];
+
+    if (symbolsCheck.checked) {
+      // add one guaranteed symbol
+      const randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+      passwordArray.push(randomSymbol);
+
+      // fill the rest with with random characters
+      while (passwordArray.length < passwordLength) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        const randomChar = characters[randomIndex];
+
+        passwordArray.push(randomChar);
       }
-  // without symbols
-  } else {
-    for (let i = 0; i < passwordLength; i++) {
-      let randomIndex1 = Math.floor(Math.random() * letters.length);
-      let randomIndex2 = Math.floor(Math.random() * letters.length);
-      passwordEl1.textContent += letters[randomIndex1];
-      passwordEl2.textContent += letters[randomIndex2];
+    } else {
+      while (passwordArray.length < passwordLength) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        const randomChar = letters[randomIndex];
+        passwordArray.push(randomChar);
+      }
+    }
+    // shuffle so the symbol isn’t always first (when symbols are on)
+    passwordArray.sort(() => Math.random() - 0.5);
+
+    // assign result to correct element
+    if (i === 0) {
+      passwordEl1.textContent = passwordArray.join("");
+    } else {
+      passwordEl2.textContent = passwordArray.join("");
     }
   }
-
-  }
+}
 passwordGenerateEl.addEventListener("click", generatePassword);
